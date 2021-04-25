19dc69542e4fdac25df7b76924a911ba64df583f
Pips <keylupet@gmail.com>
2021-04-25 16:33:15 +0800

    Test123

--- HelloWorldDotNet/Program.cs
+++ HelloWorldDotNet/Program.cs
@@ -7,7 +7,8 @@ namespace HelloWorldDotNet
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-	    Console.WriteLine("Hello World!");
+	            Console.WriteLine("Hello World!");
+                    Console.WriteLine("Test123...");
         }
     }
 }
